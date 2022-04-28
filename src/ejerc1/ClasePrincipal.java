8a898c89a04b3be47add278c878cb6670c99dd09
richi <ricardohernan.daw@ciudadescolarfp.es>
2022-04-28 13:09:00 +0200

    Inicio de proyecto

---
+++ src/ejerc1/ClasePrincipal.java
@@ -0,0 +1,10 @@
+package ejerc1;
+
+public class ClasePrincipal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Listado de alumnos 1º DAW");
+	}
+
+}
